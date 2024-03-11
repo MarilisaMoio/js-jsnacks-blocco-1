51c3c68c11e8c54a9d19af6fc5591567534405b0
MarilisaMoio <moiomarilisa@gmail.com>
2024-03-11 17:30:31 +0100

    feat: add snack-4

--- snack-4/js/script.js
+++ snack-4/js/script.js
@@ -1,1 +1,17 @@
 //Calcola la somma e la media dei primi 10 numeri.
+
+//quanti numeri vogliamo
+const numbersLength = 10;
+
+//somma
+let numberSum = 0;
+
+for (let i = 1; i <= numbersLength; i++){
+    numberSum += i;
+}
+
+//media
+let numberAverage = numberSum / numbersLength;
+
+//risultato
+console.log(numberAverage, numbersLength);
