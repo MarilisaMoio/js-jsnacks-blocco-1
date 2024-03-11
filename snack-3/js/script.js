63d6c53b9d856e9a1b8b8eee48bb96076e37ddc4
MarilisaMoio <moiomarilisa@gmail.com>
2024-03-11 17:05:16 +0100

    feat: add snack-3

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -1,3 +1,19 @@
 //Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
 
+const oddNumbers = []
+
+for (let i = 1; i <= 6; i++){
+    //input utente
+    const userNumber = parseInt(prompt("Inserisci qui un numero:"));
+
+    //controllo delle condizioni ed eventuale push
+    if (userNumber % 2 !== 0){
+        oddNumbers.push(userNumber);
+    }
+}
+
+//output finale
+console.log(oddNumbers)
+
+
 
