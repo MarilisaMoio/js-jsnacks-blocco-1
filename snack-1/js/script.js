9338735a6c00cd597ce982934a5dbe206c389c5b
MarilisaMoio <moiomarilisa@gmail.com>
2024-03-11 16:42:09 +0100

    feat: add snack-1

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,1 +1,18 @@
 //Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
+
+//array container
+const numbersArray = [];
+
+
+for (let i = 1; i <= 5; i++){
+    //input utente
+    const userNumber = parseInt(prompt("Inserisci qui un numero:"));
+
+    //controllo dell'array ed eventuale push
+    if (numbersArray.includes(userNumber) === false){
+        numbersArray.push(userNumber);
+    }
+}
+
+//output finale
+console.log(numbersArray);
